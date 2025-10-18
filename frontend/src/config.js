a470f7d45ebe5e0085c257774a65dd37f09b92e0
Meet Jaiswal <meetjaiswal.2004@gmail.com>
2025-10-19 00:24:34 +0530

    Delete frontend/src/config.js

--- frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-// Server 1 (Authentication)
-const AUTH_API_URL = "https://unaspersed-unwithstanding-journey.ngrok-free.app";
-
-// Server 2 (Prediction) 
-const PREDICT_API_URL = "https://condescending-separately-yaeko.ngrok-free.app";
-
-export { AUTH_API_URL, PREDICT_API_URL };
